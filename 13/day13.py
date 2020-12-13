ab4b8666dba83be065c9d3dd272315c9100d7c99
Chris Bier <github@cymor.com>
2020-12-12 23:04:00 -0600

    Day 13: Initial

---
+++ 13/day13.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Day 12
+#import sys
+#import re
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
+
+inputfile = open('day12input', 'r')
+
+input_list = inputfile.read().split('\n')
+
+class Navigation:
+    
